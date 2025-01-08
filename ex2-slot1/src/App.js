12aa4ecb46a3767ce30580ab2a2d3d87b2898e1a
Đoàn Xuân Sơn <limbo04112004@gmail.com>
2025-01-08 15:44:19 +0700

    change com

--- ex2-slot1/src/App.js
+++ ex2-slot1/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import AgeOfPeople from './components/People';
+import People from './components/People';
 // import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <AgeOfPeople />
+     <People />
     </div>
   );
 }
