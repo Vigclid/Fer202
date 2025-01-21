5581b86346b0d140de93e38d519b7d827a064a74
Đoàn Xuân Sơn <limbo04112004@gmail.com>
2025-01-21 15:37:09 +0700

    slot4

--- slot4/jsxbasic/src/components/PeopleList.js
+++ slot4/jsxbasic/src/components/PeopleList.js
@@ -1,7 +1,39 @@
 import {memo} from "react"
 
 
-const PeopleList = ({people}) => {
+const PeopleList = () => {
+    const people = [
+        {
+          name : "Ronaldo",
+          age : "31",
+          occupation : "Football player"
+        },
+        {
+          name : "Ngo Pham Khanh Ngoc",
+          age : "19",
+          occupation : "Student"
+        },
+        {
+          name : "Stephen Willam Hawking",
+          age : "76",
+          occupation : "Theoretical physicist"
+        },
+        {
+          name : "Doan Xuan Son",
+          age : "21",
+          occupation : "Student"
+        },
+        {
+          name : "Elon Musk",
+          age : "42",
+          occupation : "SpaceX's CEO"
+        },
+        {
+          name : "Santa",
+          age : "666",
+          occupation : "Hell's captain"
+        } 
+      ]
     return (
         <ul>
             {
--- slot4/jsxbasic/src/components/PeopleListInTable.js
+++ slot4/jsxbasic/src/components/PeopleListInTable.js
@@ -4,11 +4,14 @@ import {memo} from "react"
 const PeopleListInTable = ({people}) => {
     return (
         <table border = "2" style={{borderCollapse: "collapse"}}>
+            <thead>
             <tr>
                 <th style={{width:"400px"}}>Name</th>
                 <th>Age</th>
                 <th>Occupation</th>
             </tr>
+            </thead>
+            <tbody>
             {
                  people.map((person,index) => 
                     <tr key={index}>
@@ -18,6 +21,7 @@ const PeopleListInTable = ({people}) => {
                     </tr>
                   )
             }
+            </tbody>
         </table>
     )
 }
