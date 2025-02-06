077d42eb7fbe2ebb8f7959f140020a606ff9a4c5
Đoàn Xuân Sơn <limbo04112004@gmail.com>
2025-02-06 14:15:07 +0700

    ex8

---
+++ slot6/exercise4/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import FormRegister from './components/FormRegister';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <FormRegister />
+  );
+}
+
+export default App;
---
+++ slot6/exercise4/src/components/FormRegister.jsx
@@ -0,0 +1,88 @@
+import {memo} from 'react'
+import { Container,Row,Col,Button,InputGroup,Form } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+
+const FormRegister = () => {
+
+    return (
+        <Container className="my-5 border" >
+            <Row><h3><b>Form đặt vé máy bay</b></h3></Row>
+
+            <Row>
+                <Form.Label>Họ tên</Form.Label>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text id="basic-addon1">
+                        <FontAwesomeIcon icon={faUser} />
+                    </InputGroup.Text>
+                    <Form.Control
+                        placeholder="Họ Tên"
+                        aria-label="Username"
+                        aria-describedby="basic-addon1"
+                    />
+                    <InputGroup.Text id="basic-addon2">vnđ</InputGroup.Text>
+                </InputGroup>
+                <Form.Text className="text-muted">Phải nhật 5 ký tự, in hoa,...</Form.Text>
+            </Row>
+
+            <Row className='my-5'>
+                <Form.Label >Địa chỉ</Form.Label>
+                <InputGroup className="mb-3">
+    
+                    <Form.Control
+                        placeholder=""
+                        aria-label="Adress"
+                        aria-describedby="basic-addon1"
+                    />
+                </InputGroup>
+            </Row>
+
+            <Row className='my-5'>
+                <Col>
+                    <Form.Label >Đi từ</Form.Label>
+                    <Form.Select aria-label="Default select example">
+                        <option value="1">Hà Nội</option>
+                        <option value="2">Đà Nẵng</option>
+                        <option value="3">Hồ Chí Minh</option>
+                    </Form.Select>
+                </Col>
+
+                <Col>
+                    <Form.Label >Đến</Form.Label>
+        
+                    <Form.Select aria-label="Default select example">
+                        <option value="1">Hà Nội</option>
+                        <option value="2">Đà Nẵng</option>
+                        <option value="3">Hồ Chí Minh</option>
+                    </Form.Select>
+
+                </Col>
+            </Row>
+
+            <Row>
+            <Form>
+                <p>Chọn chiều đi (khứ hồi)</p>
+                {[
+                    { id: 'one-way', label: 'Đi' },
+                    { id: 'round-trip', label: 'Về' }
+                ].map((item) => (
+                    <div key={item.id} className="mb-3">
+                        <Form.Check 
+                            type="checkbox"
+                            id={item.id}
+                            label={item.label} 
+                        />
+                    </div>
+                ))}
+            </Form>
+            </Row>
+
+            <Row>
+                <Button variant='primary'>Đặt Vé </Button>
+            </Row>
+        </Container>
+    )
+}
+
+export default memo(FormRegister)
